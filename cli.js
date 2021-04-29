15b4055aed1184eba87ff36db9960921cbcf8ca7
Vlad Tansky <vl.tansky@gmail.com>
2021-04-29 22:35:21 +0300

    Add `--include-original` flag (#5)

--- cli.js
+++ cli.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const meow = require('meow');
 const prettyBytes = require('pretty-bytes');
 const gzipSize = require('gzip-size');
+const chalk = require('chalk');
 
 const cli = meow(`
 	Usage
@@ -11,20 +12,26 @@ const cli = meow(`
 	  $ cat <file> | gzip-size
 
 	Options
-	  --level  Compression level [0-9] (Default: 9)
-	  --raw    Display value in bytes
+	  --level             Compression level [0-9] (Default: 9)
+	  --raw               Display value in bytes
+	  --include-original  Include original size
 
 	Examples
 	  $ gzip-size unicorn.png
 	  192 kB
 	  $ gzip-size unicorn.png --raw
 	  192256
+	  $ gzip-size unicorn.png --include-original
+	  392 kB → 192 kB
 `, {
 	flags: {
 		level: {
 			type: 'number'
 		},
 		raw: {
+			type: 'boolean'
+		},
+		includeOriginal: {
 			type: 'boolean'
 		}
 	}
@@ -45,5 +52,11 @@ if (cli.flags.level) {
 }
 
 source.pipe(gzipSize.stream(options)).on('gzip-size', size => {
-	console.log(cli.flags.raw ? size : prettyBytes(size));
+	let output = cli.flags.raw ? size : prettyBytes(size);
+	if (cli.flags.includeOriginal) {
+		const {size: originalSize} = fs.statSync(input);
+		output = (cli.flags.raw ? originalSize : prettyBytes(originalSize)) + chalk.dim(' → ') + output;
+	}
+
+	console.log(output);
 });
--- readme.md
+++ readme.md
@@ -18,14 +18,17 @@ $ gzip-size --help
     $ cat <file> | gzip-size
 
   Options
-    --level  Compression level [0-9] (Default: 9)
-    --raw    Display value in bytes
+    --level             Compression level [0-9] (Default: 9)
+    --raw               Display value in bytes
+    --include-original  Include original size
 
   Examples
     $ gzip-size unicorn.png
     192 kB
     $ gzip-size unicorn.png --raw
     192256
+    $ gzip-size unicorn.png --include-original
+    392 kB → 192 kB
 ```
 
 ## Related
